6ce68c004df2ed809c8cd680adfdf35fbde0256e
Kjetil Matheussen <k.s.matheussen@notam02.no>
2015-08-01 09:13:55 +0200

    build_linux64.sh

---
+++ build_linux64.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "arg1 (COMIPLER_FLAGS) is unset";
+    exit -1
+fi
+
+set -e
+set -x
+
+
+COMPILER_FLAGS=$1
+
+SYSTEM=linux64
+
+CC=x86_64-redhat-linux-gcc
+CCC=x86_64-redhat-linux-g++
+
+COMPILER_FLAGS=$COMPILER_FLAGS ./build_lmms.sh $CC $CCC build/$SYSTEM/build_lmms build/$SYSTEM/dlls
+COMPILER_FLAGS=$COMPILER_FLAGS ./build_kokkinizita.sh $CC $CCC build/$SYSTEM/build_mcp build/$SYSTEM/dlls
